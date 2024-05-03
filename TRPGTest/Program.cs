34437430a663df76dc2c74dc36df4e8346d208e4
chojongwan <79524474+chojongwan@users.noreply.github.com>
2024-05-03 14:43:16 +0900

    이사
    이사

--- TRPGTest/Program.cs
+++ TRPGTest/Program.cs
@@ -5,9 +5,9 @@
         // 메인 메서드
         public static void Main(string[] args)
         {
-            mainMinu.ShowMainMenu();
+            mainMenu.ShowMainMenu();
         }
 
-        static MainMenu mainMinu = new MainMenu();
+        static MainMenu mainMenu = new MainMenu();
     }
 }
